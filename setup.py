cdf259c34d9ed45b82ee33598b9e3e717d44938e
Justin Babilino <jlbabilino@Justins-Air.verizon.net>
2022-06-16 09:24:27 -0400

    Converted to click

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='HelixTrajectory',
     version='0.0.0',
-    description='Triple Helix Trajectory Generation Utility',
     author='Joshua Nichols',
-    packages=['helixtrajectory', 'helixtrajectory.drive'],
+    license='MIT',
+    description='Triple Helix Trajectory Generation Utility',
+    long_description=long_description,
+    py_modules=['cli', 'app'],
+    packages=find_packages(),
     install_requires=[
         'casadi>=3.5.5',
-        'numpy>=1.19.3'
+        'numpy>=1.19.3', # TODO: try later versions
+        'click>=8.1.3'
     ],
-    include_package_data=True
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent'
+    ],
+    entry_points={
+        'console_scripts': [
+            'helixtrajectory = cli:cli'
+        ]
+    }
 )
